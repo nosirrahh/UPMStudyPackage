708c8c9789e76812b41b76a0faa8e2460614c46f
nosirrahh <nosirrahh@gmail.com>
2024-02-19 12:27:44 -0300

    Adicionado pasta Docs para teste do Github pages

---
+++ Docs/UPMStudyPackage.md
@@ -0,0 +1,2 @@
+Essa documentação foi criada utilizando o NaturalDocs.
+https://www.naturaldocs.org/
---
+++ Docs/search/keywords/007300740061.js
@@ -0,0 +1,1 @@
+﻿NDSearch.OnPrefixDataLoaded("sta",["Function"],[["Start",,[["Nosirrahh.UPMStudyPackage.HelloWorld",,,,0,"File:Runtime/Scripts/HelloWorld.cs:Nosirrahh.UPMStudyPackage.HelloWorld.Start","CSharpClass:Nosirrahh.UPMStudyPackage.HelloWorld:Start"]]]]);
